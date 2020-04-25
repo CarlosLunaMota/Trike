84cd5462a4e81e2d6a95adb3c6de2e8d4fa0ed9a
Carlos Luna Mota <CarlosLunaMota@users.noreply.github.com>
2020-04-25 13:45:26 +0200

    Add files via upload

--- trike.py
+++ trike.py
@@ -30,7 +30,7 @@
 __all__      = ["trike"]
 __author__   = "Carlos Luna-Mota"
 __license__  = "The Unlicense"
-__version__  = "20200424"
+__version__  = "20200425"
 
 import random
 
@@ -46,23 +46,24 @@ UNKNOWN  =  0   # Must be  0
 
 ### GAME LOGIC #################################################################
 
-def legal_moves(board, last_move):
+def legal_moves(board):
     """Returns a list of legal moves from the current position."""
 
-    # You can play anywhere in the first turn:
     size = board["size"]
-    if not last_move: return [(r,c) for r in range(size) for c in range(r+1)]
+    pawn = board["pawn"]
+
+    # You can play anywhere in the first turn:
+    if not pawn: return [(r,c) for r in range(size) for c in range(r+1)]
 
     # After the first turn the neutral pawn can move in any direction...
-    (r,c)  = last_move                  #             (0, 0)
+    moves  = []                         #             (0, 0)
     deltas = (    (-1,-1),(-1, 0),      #         (1, 0), (1, 1)
               ( 0,-1),        ( 0, 1),  #     (2, 0), (2, 1), (2, 2)
                   ( 1, 0),( 1, 1))      # (3, 0), (3, 1), (3, 2), (3, 3)
 
     # ...but cannot move onto, or jump over occupied points:
-    moves  = []
     for (dr,dc) in deltas:
-        rr,cc = r+dr, c+dc
+        rr,cc = pawn[0]+dr, pawn[1]+dc
         while 0 <= cc <= rr < size and board[(rr,cc)] == EMPTY:
            moves.append((rr,cc))
            rr += dr
@@ -70,11 +71,14 @@ def legal_moves(board, last_move):
 
     return moves
 
-def who_wins(board, last_move):
+def who_wins(board):
     """If the game is over, returns the winner. Otherwise, returns EMPTY."""
 
+    # If nobody has played yet, the game cannot be over:
+    if not board["pawn"]: return EMPTY
+
     # For each cell adjacent to, or underneath, the pawn...
-    (r,c) = last_move                       #             (0, 0)
+    (r,c) = board["pawn"]                   #             (0, 0)
     neighborhood = (  (r-1,c-1),(r-1,c),    #         (1, 0), (1, 1)
                     (r,c-1),(r,c),(r,c+1),  #     (2, 0), (2, 1), (2, 2)
                       (r+1,c),(r+1,c+1))    # (3, 0), (3, 1), (3, 2), (3, 3)
@@ -95,9 +99,9 @@ def who_wins(board, last_move):
 
 ### AI #########################################################################
 
-def get_value(board, last_move, depth):
+def get_value(board, depth):
     """
-    A simple recursive negamax solver for Tryke.
+    A simple recursive negamax solver for Tryke (negamin, indeed).
 
     Returns the value of the current position for the player that just moved:
 
@@ -108,33 +112,39 @@ def get_value(board, last_move, depth):
     """
 
     # Check if the game is already over or if we have run out of depth:
-    last_turn = board[last_move]
-    is_over   = who_wins(board, last_move)
-    if is_over or depth == 0: return is_over * last_turn
+    pawn    = board["pawn"]
+    turn    = board[pawn]
+    is_over = who_wins(board)
+    if is_over or depth == 0: return is_over * turn
 
     # Otherwise, explore the game tree:
     value = WINNING
-    for move in legal_moves(board, last_move):
-        board[move] = -last_turn                                    # Make move
-        value       = min(value, -get_value(board, move, depth-1))  # Get value
-        board[move] = EMPTY                                         # Undo move
-        if value == LOSING: break                                   # Prunning
+    for move in legal_moves(board):
+        board[move]   = -turn                                   # Place checker
+        board["pawn"] =  move                                   # Move pawn
+        value         = min(value, -get_value(board, depth-1))  # Get value
+        board[move]   = EMPTY                                   # Remove checker
+        board["pawn"] = pawn                                    # Move pawn back
+        if value == LOSING: break                               # Prunning
 
     return value
 
-def get_computer_move(board, last_move, AI_level):
-    """Return the best move the AI is able to find at the current AI_level"""
+def get_computer_move(board, AI_level):
+    """Return the best move the AI is able to find at the current AI_level."""
 
     winning = []
     unknown = []
     losing  = []
+    pawn    = board["pawn"]
 
     # Classify all legal moves:
-    for move in legal_moves(board, last_move):
+    for move in legal_moves(board):
 
-        board[move] = COMPUTER                          # Make move
-        value       = get_value(board, move, AI_level)  # Get value
-        board[move] = EMPTY                             # Undo move
+        board[move]   = COMPUTER                    # Place a checker
+        board["pawn"] = move                        # Move the pawn
+        value         = get_value(board, AI_level)  # Get value
+        board[move]   = EMPTY                       # Remove the checker
+        board["pawn"] = pawn                        # Move back the pawn
 
         if   value == WINNING: winning.append(move)
         elif value == UNKNOWN: unknown.append(move)
@@ -143,7 +153,7 @@ def get_computer_move(board, last_move, AI_level):
     # Report results:
     cells = ((r,c) for r in range(board["size"]) for c in range(r+1))
     name  = {cell:str(i+1) for i,cell in enumerate(cells)}
-    print("\n AI(" + str(AI_level) + ") found...\n")
+    print("\n AI("+(str(AI_level) if AI_level > 0 else "infinity")+") found:\n")
     print("  * Winning moves: " + ", ".join(name[cell] for cell in winning))
     print("  * Unknown moves: " + ", ".join(name[cell] for cell in unknown))
     print("  * Losing  moves: " + ", ".join(name[cell] for cell in losing))
@@ -159,13 +169,13 @@ def get_computer_move(board, last_move, AI_level):
 try: input = raw_input  # Use Python 2 raw_input function
 except NameError: pass  # Use Python 3 input function
 
-def get_player_move(board, last_move):
-    """Ask the user which move wants to make. Shows legal moves."""
+def get_player_move(board):
+    """Ask the user which move wants to make."""
 
     legal = dict()
     cells = ((r,c) for r in range(board["size"]) for c in range(r+1))
     for i,cell in enumerate(cells):
-        if cell in legal_moves(board, last_move): legal[i+1] = cell
+        if cell in legal_moves(board): legal[i+1] = cell
 
     question = "\n What is your next move?\n\n "
     move     = input(question)
@@ -173,11 +183,12 @@ def get_player_move(board, last_move):
 
     return legal[int(move)]
 
-def show(board, last_move):
-    """Prints board on the screen, shows last move and legal moves."""
+def show(board):
+    """Prints board on the screen, highlighting last move & legal moves."""
 
     size  = board["size"]
-    moves = legal_moves(board, last_move)
+    pawn  = board["pawn"]
+    moves = legal_moves(board)
 
     print(" " + "_" * (6*size + 6) + "\n")
 
@@ -186,10 +197,10 @@ def show(board, last_move):
         row  = [" " *   (size-r)]
         info = [" " * 3*(size-r)]
         for c in range(r+1):
-            if (r,c) == last_move: row.append(".XO"[board[(r,c)]])
-            else:                  row.append(".xo"[board[(r,c)]])
-            if (r,c) in moves:     info.append("{:4d}".format(total+c))
-            else:                  info.append("   ·")
+            if (r,c) == pawn:  row.append(".XO"[board[(r,c)]])
+            else:              row.append(".xo"[board[(r,c)]])
+            if (r,c) in moves: info.append("{:4d}".format(total+c))
+            else:              info.append("   ·")
         print(" " + " ".join(row) + "".join(info))
         total += r+1
 
@@ -204,36 +215,42 @@ def trike(size, AI_level):
      * Parameter `size`     (integer >= 1) controls the boad size.
      * Parameter `AI_level` (integer >= 0) controls the ply-depth of the AI.
 
+    Any negative value for `AI_level` is interpreted as `infinity`.
+
     """
+
+    # Validate parameter:
+    assert(size > 0)
 
     # Initialize game:
     turn          = random.choice((PLAYER, COMPUTER))
     cells         = tuple((r,c) for r in range(size) for c in range(r+1))
     board         = {cell:EMPTY for cell in cells}
     board["size"] = size
-    move          = None
-
+    board["pawn"] = None
+    
     if turn == PLAYER: print("\n You play first")
     else:              print("\n The computer plays first")
     
     # Play the game:
     while True:
 
-        show(board, move)
+        show(board)
 
-        if turn == PLAYER: move = get_player_move(board, move)
-        else:              move = get_computer_move(board, move, AI_level)
+        if turn == PLAYER: move = get_player_move(board)
+        else:              move = get_computer_move(board, AI_level)
 
-        board[move] =  turn                 # Play the move
-        turn        = -turn                 # Change turn
-        is_over     = who_wins(board, move) # Check if it is over
+        board[move]   =  turn               # Place the checker
+        board["pawn"] =  move               # Move the pawn
+        turn          = -turn               # Change turn
+        is_over       =  who_wins(board)    # Check if it is over
 
         if is_over:
-            show(board, move)
+            show(board)
             if is_over == PLAYER: print("\n You have won!")
             else:                 print("\n You have lost!")
             break
 
-if __name__ == "__main__": trike(size=5, AI_level=15)
+if __name__ == "__main__": trike(size=9, AI_level=5)
 
 ################################################################################
